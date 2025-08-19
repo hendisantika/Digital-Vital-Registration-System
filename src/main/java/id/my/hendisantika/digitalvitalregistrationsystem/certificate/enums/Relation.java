bbea39842917a524a90f4ebb2a7abf42e752f02b
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 06:20:59 +0700

    feat: Add Relation Enum🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/certificate/enums/Relation.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.certificate.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/08/25
+ * Time: 06.20
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Relation {
+    FATHER,
+    MOTHER,
+    SPOUSE,
+    SON,
+    DAUGHTER,
+    BROTHER,
+    SISTER,
+    UNCLE,
+    AUNT,
+    GRANDPARENT,
+    OTHER
+}
