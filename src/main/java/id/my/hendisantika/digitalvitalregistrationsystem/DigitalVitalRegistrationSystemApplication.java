de25821a1d206d5c34a4c60fe4ec2fe432d6c914
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 05:03:33 +0700

    feat: Init Project🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/DigitalVitalRegistrationSystemApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.digitalvitalregistrationsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalVitalRegistrationSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DigitalVitalRegistrationSystemApplication.class, args);
+    }
+
+}
