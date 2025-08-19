ac182d14b25e5ffc3fa7df5279064afacb00e9a4
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 06:23:39 +0700

    feat: Add BirthCertificateReportService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/certificate/service/BirthCertificateReportService.java
@@ -0,0 +1,23 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.certificate.service;
+
+import id.my.hendisantika.digitalvitalregistrationsystem.certificate.repository.CertificateFileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/08/25
+ * Time: 06.23
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class BirthCertificateReportService {
+    private final CertificateFileRepository certificateFileRepository;
+    private final SignatureKeysService signatureKeysService;
+}
