291c36f4771200aa8a1dd66d0e9b4539cd38a60f
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 06:36:46 +0700

    feat: Add deleteBirthCertificateRequestById in BirthCertificateController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/certificate/controller/BirthCertificateController.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/certificate/controller/BirthCertificateController.java
@@ -10,6 +10,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -89,5 +90,11 @@ public class BirthCertificateController {
     @GetMapping("/rejected-count")
     public ResponseEntity<Long> countRejectedRequest() {
         return ResponseEntity.ok(birthCertificateService.countRejectedRequest());
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public ResponseEntity<Void> deleteBirthCertificateRequestById(@PathVariable Long id) {
+        birthCertificateService.deleteBirthCertificateRequestById(id);
+        return ResponseEntity.noContent().build();
     }
 }
