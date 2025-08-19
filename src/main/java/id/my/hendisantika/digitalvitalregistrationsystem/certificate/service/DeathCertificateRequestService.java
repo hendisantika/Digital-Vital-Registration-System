1020b773199975ba779b00197853505943cd8977
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 06:32:31 +0700

    feat: Add saveDeathCertificateRequest in DeathCertificateRequestService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/certificate/service/DeathCertificateRequestService.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/certificate/service/DeathCertificateRequestService.java
@@ -1,7 +1,14 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.certificate.service;
 
+import id.my.hendisantika.digitalvitalregistrationsystem.certificate.enums.CertificateStatus;
+import id.my.hendisantika.digitalvitalregistrationsystem.certificate.model.DeathCertificateRequest;
+import id.my.hendisantika.digitalvitalregistrationsystem.certificate.repository.DeathCertificateRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +28,20 @@ public class DeathCertificateRequestService {
     private final DeathCertificateRepository deathCertificateRepository;
     private final DeathCertificateReportService deathCertificateReportService;
 
+    public DeathCertificateRequest saveDeathCertificateRequest(DeathCertificateRequest deathCertificateRequest) {
+        if (deathCertificateRepository.existsByDeceasedNameAndDateOfBirthAndDeceasedDateAndRequestedBy(deathCertificateRequest.getDeceasedName(), deathCertificateRequest.getDateOfBirth(),
+                deathCertificateRequest.getDeceasedDate(), deathCertificateRequest.getRequestedBy())) {
+            throw new RuntimeException("Death certificate already exists");
+        }
+
+        Long citizenId = deathCertificateRequest.getRequestedBy().getId();
+        Citizen citizen = citizenRepository.findById(citizenId)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Citizen not found with ID: " + citizenId));
+        deathCertificateRequest.setRequestedBy(citizen);
+        deathCertificateRequest.setCertificateStatus(CertificateStatus.PENDING);
+        deathCertificateRequest.setRequestedAt(LocalDateTime.now());
+
+
+        return deathCertificateRepository.save(deathCertificateRequest);
+    }
 }
