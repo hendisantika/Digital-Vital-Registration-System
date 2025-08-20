7c365154a33157954609c4066afad20724a98327
hendisantika <hendisantika@yahoo.co.id>
2025-08-21 05:52:27 +0700

    feat: Add DocumentService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/service/DocumentService.java
@@ -0,0 +1,29 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.citizen.service;
+
+import id.my.hendisantika.digitalvitalregistrationsystem.certificate.repository.CertificateFileRepository;
+import id.my.hendisantika.digitalvitalregistrationsystem.citizen.repository.CitizenDocumentRepository;
+import id.my.hendisantika.digitalvitalregistrationsystem.citizen.repository.CitizenRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/08/25
+ * Time: 05.51
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class DocumentService {
+    private final CitizenDocumentRepository citizenDocumentRepository;
+    private final CitizenRepository citizenRepository;
+    private final CertificateFileRepository certificateFileRepository;
+    private final NotificationService notificationService;
+}
