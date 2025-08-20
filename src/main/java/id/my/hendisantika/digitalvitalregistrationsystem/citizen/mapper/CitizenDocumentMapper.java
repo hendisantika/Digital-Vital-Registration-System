220bb4f8aecd3f1b81b0fe056f902e79f90ab8d9
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 08:56:24 +0700

    feat: Add toResponseDto in CitizenDocumentMapper🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/mapper/CitizenDocumentMapper.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/mapper/CitizenDocumentMapper.java
@@ -1,5 +1,8 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.citizen.mapper;
 
+import id.my.hendisantika.digitalvitalregistrationsystem.citizen.dto.CitizenDocumentResponseDto;
+import id.my.hendisantika.digitalvitalregistrationsystem.citizen.model.CitizenDocument;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : Digital-Vital-Registration-System
@@ -12,4 +15,18 @@ package id.my.hendisantika.digitalvitalregistrationsystem.citizen.mapper;
  * To change this template use File | Settings | File Templates.
  */
 public class CitizenDocumentMapper {
+    public static CitizenDocumentResponseDto toResponseDto(CitizenDocument document) {
+        return CitizenDocumentResponseDto.builder()
+                .id(document.getId())
+                .fileName(document.getFileName())
+                .documentType(document.getDocumentType())
+                .updatedAt(document.getUploadDate())
+                .citizenId(document.getCitizen().getId())
+                .verifiedBy(document.getVerifiedBy())
+                .verifiedDate(document.getVerifiedDate())
+                .createdAt(document.getCreatedAt())
+                .fileData(document.getFileData())
+                .build();
+    }
+
 }
