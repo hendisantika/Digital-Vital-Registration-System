8304b759bc9db49db983703e05cccf87ef279025
hendisantika <hendisantika@yahoo.co.id>
2025-08-21 06:05:06 +0700

    feat: Add KafkaNotificationProducer🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/kafka/producer/KafkaNotificationProducer.java
@@ -0,0 +1,31 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.kafka.producer;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Service;
+
+import javax.management.Notification;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/08/25
+ * Time: 06.04
+ * To change this template use File | Settings | File Templates.
+ */
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class KafkaNotificationProducer {
+    private final KafkaTemplate<String, Notification> kafkaTemplate;
+
+    public void sendNotification(Notification notification) {
+        kafkaTemplate.send("notification-topic", notification);
+        log.info("Notification sent to topic {}", notification.getId());
+    }
+}
