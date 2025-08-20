6eae807000b4f0e88f92c76ce600942de624d0e4
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 08:56:09 +0700

    feat: Add CitizenDocumentMapper🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/mapper/CitizenDocumentMapper.java
@@ -0,0 +1,15 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.citizen.mapper;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/08/25
+ * Time: 08.55
+ * To change this template use File | Settings | File Templates.
+ */
+public class CitizenDocumentMapper {
+}
