0dda148fc9b5c32499abeecf5cd42a5af02d00fc
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 08:51:45 +0700

    feat: Add CitizenStatus Enum🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/enums/CitizenStatus.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.citizen.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/08/25
+ * Time: 08.51
+ * To change this template use File | Settings | File Templates.
+ */
+public enum CitizenStatus {
+    PENDING,
+    APPROVED,
+    REJECTED
+}
