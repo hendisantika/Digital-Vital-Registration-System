d6b6a5c5922e6009bf6804a967dd94686c9bdf93
hendisantika <hendisantika@yahoo.co.id>
2025-08-21 06:21:50 +0700

    feat: Add saveMarriageCertificateRequest in MarriageCertificateController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/marriage/controller/MarriageCertificateController.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/marriage/controller/MarriageCertificateController.java
@@ -1,11 +1,19 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.marriage.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import id.my.hendisantika.digitalvitalregistrationsystem.certificate.repository.CertificateFileRepository;
+import id.my.hendisantika.digitalvitalregistrationsystem.marriage.model.MarriageCertificateRequest;
 import id.my.hendisantika.digitalvitalregistrationsystem.marriage.service.MarriageCertificateRequestService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Created by IntelliJ IDEA.
@@ -25,4 +33,15 @@ public class MarriageCertificateController {
     private final MarriageCertificateRequestService marriageCertificateRequestService;
     private final ObjectMapper objectMapper;
     private final CertificateFileRepository certificateFileRepository;
+
+    @PostMapping(value = "/save", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<MarriageCertificateRequest> saveMarriageCertificateRequest(
+            @RequestPart("request") String requestJson,
+            @RequestPart(value = "marriagePhoto", required = true) MultipartFile marriagePhoto,
+            @RequestPart(value = "wardOfficeFile", required = true) MultipartFile wardOfficeFile
+    ) throws JsonProcessingException {
+        MarriageCertificateRequest request = objectMapper.readValue(requestJson, MarriageCertificateRequest.class);
+        MarriageCertificateRequest save = marriageCertificateRequestService.save(request, marriagePhoto, wardOfficeFile);
+        return new ResponseEntity<>(save, HttpStatus.CREATED);
+    }
 }
