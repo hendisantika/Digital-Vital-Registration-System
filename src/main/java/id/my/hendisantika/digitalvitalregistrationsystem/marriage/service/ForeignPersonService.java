edf63661b7f723ab9cafc05cdc4abdebd8d17471
hendisantika <hendisantika@yahoo.co.id>
2025-08-21 06:13:16 +0700

    feat: Add save in ForeignPersonService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/marriage/service/ForeignPersonService.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/marriage/service/ForeignPersonService.java
@@ -1,8 +1,13 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.marriage.service;
 
+import id.my.hendisantika.digitalvitalregistrationsystem.marriage.model.ForeignPerson;
 import id.my.hendisantika.digitalvitalregistrationsystem.marriage.repository.ForeignPersonRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.util.Base64;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +24,33 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class ForeignPersonService {
     private final ForeignPersonRepository foreignPersonRepository;
+
+    public ForeignPerson save(ForeignPerson foreignPerson,
+                              MultipartFile passportPhoto,
+                              MultipartFile personPhoto,
+                              MultipartFile personCitizenship
+    ) {
+        try {
+
+            if (passportPhoto != null && !personPhoto.isEmpty()) {
+                foreignPerson.setPassportFileData(Base64.getEncoder().encodeToString(passportPhoto.getBytes()));
+                foreignPerson.setPassportFileName(passportPhoto.getOriginalFilename());
+
+            }
+            if (personPhoto != null && !personPhoto.isEmpty()) {
+                foreignPerson.setPhotoFileData(Base64.getEncoder().encodeToString(personPhoto.getBytes()));
+                foreignPerson.setPhotoFileName(personPhoto.getOriginalFilename());
+
+            }
+
+            if (personCitizenship != null && !personCitizenship.isEmpty()) {
+                foreignPerson.setPersonCitizenshipFileData(Base64.getEncoder().encodeToString(personCitizenship.getBytes()));
+                foreignPerson.setPersonCitizenshipFileName(personCitizenship.getOriginalFilename());
+            }
+            return foreignPersonRepository.save(foreignPerson);
+
+        } catch (IOException e) {
+            throw new RuntimeException("Error while saving foreign person", e);
+        }
+    }
 }
