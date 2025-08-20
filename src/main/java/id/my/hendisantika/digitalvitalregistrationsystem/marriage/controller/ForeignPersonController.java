b81e97001ee639a31a4652bd845b9b60a0ba73ae
hendisantika <hendisantika@yahoo.co.id>
2025-08-21 06:20:50 +0700

    feat: Add save in ForeignPersonController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/marriage/controller/ForeignPersonController.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/marriage/controller/ForeignPersonController.java
@@ -1,10 +1,17 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.marriage.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import id.my.hendisantika.digitalvitalregistrationsystem.marriage.model.ForeignPerson;
 import id.my.hendisantika.digitalvitalregistrationsystem.marriage.service.ForeignPersonService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 /**
  * Created by IntelliJ IDEA.
@@ -23,4 +30,15 @@ import org.springframework.web.bind.annotation.RestController;
 public class ForeignPersonController {
     private final ForeignPersonService foreignPersonService;
     private final ObjectMapper objectMapper;
+
+    @PostMapping("/save")
+    public ResponseEntity<ForeignPerson> save(@RequestPart("foreignPerson") String foreignPersonJson,
+                                              @RequestPart(required = true) MultipartFile passportPhoto,
+                                              @RequestPart(required = true) MultipartFile personPhoto,
+                                              @RequestPart(required = true) MultipartFile personCitizenship) throws JsonProcessingException {
+        ForeignPerson foreignPerson = objectMapper.readValue(foreignPersonJson, ForeignPerson.class);
+
+        ForeignPerson save = foreignPersonService.save(foreignPerson, passportPhoto, personPhoto, personCitizenship);
+        return ResponseEntity.status(HttpStatus.CREATED).body(save);
+    }
 }
