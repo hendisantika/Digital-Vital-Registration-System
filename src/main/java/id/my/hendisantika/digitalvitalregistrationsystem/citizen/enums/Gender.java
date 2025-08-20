54f773a9c860b9df033dc685886c47fa1d84f6dc
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 08:52:22 +0700

    feat: Add Gender Enum🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/enums/Gender.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.citizen.enums;
+
+import lombok.Getter;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 20/08/25
+ * Time: 08.52
+ * To change this template use File | Settings | File Templates.
+ */
+@Getter
+public enum Gender {
+    MALE,
+    FEMALE,
+    OTHERS
+}
