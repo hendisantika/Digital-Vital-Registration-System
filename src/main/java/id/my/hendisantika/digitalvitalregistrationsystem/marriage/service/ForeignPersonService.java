ece2a5caaf5cbe7f96aca73207b9735ad560f6d8
hendisantika <hendisantika@yahoo.co.id>
2025-08-21 06:12:54 +0700

    feat: Add ForeignPersonService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/marriage/service/ForeignPersonService.java
@@ -0,0 +1,22 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.marriage.service;
+
+import id.my.hendisantika.digitalvitalregistrationsystem.marriage.repository.ForeignPersonRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 21/08/25
+ * Time: 06.12
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+@RequiredArgsConstructor
+public class ForeignPersonService {
+    private final ForeignPersonRepository foreignPersonRepository;
+}
