d65677172ab90439fb6341cc7c348d956be2e582
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 08:57:38 +0700

    feat: Add mapToDto in CitizenDtoMapper🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/mapper/CitizenDtoMapper.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/mapper/CitizenDtoMapper.java
@@ -1,5 +1,8 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.citizen.mapper;
 
+import id.my.hendisantika.digitalvitalregistrationsystem.citizen.dto.CitizenResponseDto;
+import id.my.hendisantika.digitalvitalregistrationsystem.citizen.model.Citizen;
+
 /**
  * Created by IntelliJ IDEA.
  * Project : Digital-Vital-Registration-System
@@ -12,4 +15,33 @@ package id.my.hendisantika.digitalvitalregistrationsystem.citizen.mapper;
  * To change this template use File | Settings | File Templates.
  */
 public class CitizenDtoMapper {
+    public static CitizenResponseDto mapToDto(Citizen citizen) {
+        return CitizenResponseDto.builder()
+                .id(citizen.getId())
+                .firstName(citizen.getFirstName())
+                .middleName(citizen.getMiddleName())
+                .lastName(citizen.getLastName())
+                .dateOfBirth(citizen.getDateOfBirth())
+                .phoneNo(citizen.getPhoneNo())
+                .fatherName(citizen.getFatherName())
+                .motherName(citizen.getMotherName())
+                .grandfatherName(citizen.getGrandfatherName())
+                .grandmotherName(citizen.getGrandmotherName())
+                .nationality(citizen.getNationality())
+                .district(citizen.getDistrict())
+                .municipality(citizen.getMunicipality())
+                .wardNo(citizen.getWardNo())
+                .gender(citizen.getGender())
+                .tole(citizen.getTole())
+                .spouseName(citizen.getSpouseName())
+                .verifiedBy(citizen.getVerifiedBy())
+                .verifiedDate(citizen.getVerifiedDate())
+                .createdAt(citizen.getCreatedAt())
+                .reasonForRejection(citizen.getReasonForRejection())
+                .status(citizen.getStatus())
+                .marriageStatus(citizen.getMarriageStatus())
+                .citizenshipNumber(citizen.getCitizenshipNumber())
+                .updatedAt(citizen.getUpdatedAt())
+                .build();
+    }
 }
