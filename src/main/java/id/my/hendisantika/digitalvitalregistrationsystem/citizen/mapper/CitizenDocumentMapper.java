3b9a790f9ba2ac818091ccf28b76c54a779ee4d1
hendisantika <hendisantika@yahoo.co.id>
2025-08-20 08:56:41 +0700

    feat: Add mapToEntity in CitizenDocumentMapper🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/mapper/CitizenDocumentMapper.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/citizen/mapper/CitizenDocumentMapper.java
@@ -1,7 +1,13 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.citizen.mapper;
 
+import id.my.hendisantika.digitalvitalregistrationsystem.citizen.dto.CitizenDocumentRequestDto;
 import id.my.hendisantika.digitalvitalregistrationsystem.citizen.dto.CitizenDocumentResponseDto;
+import id.my.hendisantika.digitalvitalregistrationsystem.citizen.model.Citizen;
 import id.my.hendisantika.digitalvitalregistrationsystem.citizen.model.CitizenDocument;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Base64;
 
 /**
  * Created by IntelliJ IDEA.
@@ -29,4 +35,20 @@ public class CitizenDocumentMapper {
                 .build();
     }
 
+
+    public static CitizenDocument mapToEntity(CitizenDocumentRequestDto dto, Citizen citizen) throws IOException {
+        return CitizenDocument.builder()
+                .citizen(citizen)
+                .documentType(dto.getDocumentType())
+                .fileData(Base64.getEncoder().encodeToString(dto.getFile().getBytes()))
+                .fileName(dto.getFile().getOriginalFilename())
+                .uploadDate(LocalDate.now())
+                .verifiedBy(dto.getVerifiedBy())
+                .verifiedDate(dto.getVerifiedDate())
+                .createdAt(LocalDate.now())
+                //.updatedAt(LocalDate.now())
+
+                .deathCertificateRequest(dto.getDeathCertificateRequestId())
+                .build();
+    }
 }
