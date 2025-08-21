c66be08004842e5671980532934d3dafcc9477a8
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:41:59 +0700

    feat: Add Role Enum🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/staff/enums/Role.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.staff.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 22/08/25
+ * Time: 05.41
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Role {
+    CITIZEN,
+    ADMIN,
+    SUPERADMIN
+}
