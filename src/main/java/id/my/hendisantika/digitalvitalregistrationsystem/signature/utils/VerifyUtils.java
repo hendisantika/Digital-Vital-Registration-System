0493ad4092bdd1953e5a8b2c0d26d1a4787db9fc
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:35:09 +0700

    feat: Add VerifyUtils🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/signature/utils/VerifyUtils.java
@@ -0,0 +1,18 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.signature.utils;
+
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 22/08/25
+ * Time: 05.34
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class VerifyUtils {
+}
