26159e41c4369cead43b6e39f8f8b0328df39a54
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:38:39 +0700

    feat: Add sendOtp in OtpController🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/whatsapp/controller/OtpController.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/whatsapp/controller/OtpController.java
@@ -2,8 +2,14 @@ package id.my.hendisantika.digitalvitalregistrationsystem.whatsapp.controller;
 
 import id.my.hendisantika.digitalvitalregistrationsystem.whatsapp.service.WhatsappService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
+import java.util.Random;
 
 /**
  * Created by IntelliJ IDEA.
@@ -26,6 +32,23 @@ public class OtpController {
     // Validate Nepal phone number
     public boolean isValidNepalPhoneNumber(String number) {
         return number != null && number.matches("^\\+9779[78]\\d{8}$");
+    }
+
+    @PostMapping("/send")
+    public ResponseEntity<String> sendOtp(@RequestBody Map<String, String> request) {
+        String phone = request.get("phone");
+        if (!isValidNepalPhoneNumber(phone)) {
+            return ResponseEntity.badRequest().body("Invalid Nepal phone number format.");
+        }
+
+        // Generate 6-digit OTP
+        String otp = String.format("%06d", new Random().nextInt(900000) + 100000);
+
+        // TODO: Save OTP in DB/cache with expiry for verification later
+
+        whatsappService.sendOtp(phone, otp);
+
+        return ResponseEntity.ok("OTP sent successfully to WhatsApp.");
     }
 
 }
