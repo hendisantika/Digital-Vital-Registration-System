7812d7506a712026bad6dcb36817fd504fd57153
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:43:57 +0700

    feat: Add StaffUserDtoMapper🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/staff/mapper/StaffUserDtoMapper.java
@@ -0,0 +1,20 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.staff.mapper;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 22/08/25
+ * Time: 05.43
+ * To change this template use File | Settings | File Templates.
+ */
+@Component
+@RequiredArgsConstructor
+public class StaffUserDtoMapper {
+}
