fd967bfde9c15581b583c960af152f75184c5863
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:22:11 +0700

    feat: Add NotificationEvent🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/notification/enums/NotificationEvent.java
@@ -0,0 +1,14 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.notification.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 22/08/25
+ * Time: 05.22
+ * To change this template use File | Settings | File Templates.
+ */
+public enum NotificationEvent {REVIEWING, VERIFICATION_APPROVED, VERIFICATION_REJECTED, CERTIFICATE_ISSUED}
