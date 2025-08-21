2ffef5d0546375a0d151da49ea3c256b53fd6b9c
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:30:52 +0700

    feat: Add DigitalSignatureService🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/signature/service/DigitalSignatureService.java
@@ -0,0 +1,24 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.signature.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 22/08/25
+ * Time: 05.30
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class DigitalSignatureService {
+
+    private static final Logger LOG = LoggerFactory.getLogger(DigitalSignatureService.class);
+    private final SignatureKeysService signatureKeysService;
+
+}
