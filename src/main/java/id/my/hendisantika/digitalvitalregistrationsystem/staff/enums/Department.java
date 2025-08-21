f10b94a3d14e0a04c901bf1426043dc6138ce1b2
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:41:42 +0700

    feat: Add Department Enum🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/staff/enums/Department.java
@@ -0,0 +1,21 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.staff.enums;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 22/08/25
+ * Time: 05.41
+ * To change this template use File | Settings | File Templates.
+ */
+public enum Department {
+    BIRTH_DEPARTMENT,
+    DEATH_DEPARTMENT,
+    MARRIAGE_DEPARTMENT,
+    CITIZEN_DEPARTMENT,
+    ADMINISTRATION_DEPARTMENT,
+    HEAD_OF_DEPARTMENT,
+}
