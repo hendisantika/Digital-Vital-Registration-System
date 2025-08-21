c270ce329a9fdb1875fcd867b141bc97dbdf9a72
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:32:53 +0700

    feat: Add getPublicKey in SignatureKeysService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/signature/service/SignatureKeysService.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/signature/service/SignatureKeysService.java
@@ -7,7 +7,10 @@ import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
 
 /**
  * Created by IntelliJ IDEA.
@@ -42,5 +45,10 @@ public class SignatureKeysService {
     private SignatureKeys getLatestKeys() {
         return signatureKeysRepository.findTopByOrderByIdDesc()
                 .orElseThrow(() -> new RuntimeException("No signature keys found"));
+    }
+
+    @Cacheable(value = "publicKey")
+    public BigInteger getPublicKey() {
+        return new BigInteger(getLatestKeys().getPublicKey());
     }
 }
