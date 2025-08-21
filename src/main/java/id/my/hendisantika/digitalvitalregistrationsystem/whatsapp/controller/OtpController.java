a1e78385db7d93053e36cc642fe7b13b9515faf2
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:38:10 +0700

    feat: Add OtpController🫸🌀✏️📗🐧🐳⬆

---
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/whatsapp/controller/OtpController.java
@@ -0,0 +1,26 @@
+package id.my.hendisantika.digitalvitalregistrationsystem.whatsapp.controller;
+
+import id.my.hendisantika.digitalvitalregistrationsystem.whatsapp.service.WhatsappService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : Digital-Vital-Registration-System
+ * User: hendisantika
+ * Link: s.id/hendisantika
+ * Email: hendisantika@yahoo.co.id
+ * Telegram : @hendisantika34
+ * Date: 22/08/25
+ * Time: 05.37
+ * To change this template use File | Settings | File Templates.
+ */
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/v1/whatsapp/otp")
+public class OtpController {
+
+    private final WhatsappService whatsappService;
+
+}
