ff8b22cf9e940fe5d3fbd348b4b44027dbfecef0
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:34:18 +0700

    feat: Add sha256 in SignatureUtils🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/signature/utils/SignatureUtils.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/signature/utils/SignatureUtils.java
@@ -22,4 +22,8 @@ public class SignatureUtils {
         MessageDigest md = MessageDigest.getInstance("SHA-256");
         return md.digest(input);
     }
+
+    public static byte[] sha256(String input) throws NoSuchAlgorithmException {
+        return sha256(input.getBytes());
+    }
 }
