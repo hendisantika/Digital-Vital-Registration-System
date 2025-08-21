806057e650fa9f12e3739e46dfae9c78f42ac3a8
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:31:10 +0700

    feat: Add signCertificate in DigitalSignatureService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/signature/service/DigitalSignatureService.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/signature/service/DigitalSignatureService.java
@@ -1,8 +1,12 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.signature.service;
 
+import org.aspectj.weaver.SignatureUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+
+import java.math.BigInteger;
+import java.security.NoSuchAlgorithmException;
 
 /**
  * Created by IntelliJ IDEA.
@@ -21,4 +25,10 @@ public class DigitalSignatureService {
     private static final Logger LOG = LoggerFactory.getLogger(DigitalSignatureService.class);
     private final SignatureKeysService signatureKeysService;
 
+    // New version: sign raw bytes
+    public String signCertificate(byte[] certificateBytes) throws NoSuchAlgorithmException {
+        BigInteger message = new BigInteger(1, certificateBytes); // Avoid negative numbers
+        BigInteger signature = SignatureUtils.sign(message.toByteArray(), signatureKeysService.getPrivateKey(), signatureKeysService.getModulus());
+        return signature.toString();
+    }
 }
