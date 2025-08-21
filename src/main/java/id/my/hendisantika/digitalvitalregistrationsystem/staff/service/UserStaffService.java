c218fa6eae1554d062f0ca8249f6de4a2c98a9f5
hendisantika <hendisantika@yahoo.co.id>
2025-08-22 05:47:27 +0700

    feat: Add createSuperAdmin in UserStaffService🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/staff/service/UserStaffService.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/staff/service/UserStaffService.java
@@ -1,10 +1,19 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.staff.service;
 
 import id.my.hendisantika.digitalvitalregistrationsystem.jwt.utils.JwtUtil;
+import id.my.hendisantika.digitalvitalregistrationsystem.staff.dto.StaffUserRequestDto;
+import id.my.hendisantika.digitalvitalregistrationsystem.staff.dto.StaffUserResponseDto;
+import id.my.hendisantika.digitalvitalregistrationsystem.staff.enums.Role;
+import id.my.hendisantika.digitalvitalregistrationsystem.staff.enums.Status;
+import id.my.hendisantika.digitalvitalregistrationsystem.staff.mapper.StaffUserDtoMapper;
+import id.my.hendisantika.digitalvitalregistrationsystem.staff.model.StaffUser;
 import id.my.hendisantika.digitalvitalregistrationsystem.staff.repository.StaffUserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -24,4 +33,45 @@ public class UserStaffService {
     private final UserRepository userRepository;
     private final BCryptPasswordEncoder passwordEncoder;
     private final JwtUtil jwtUtil;
+
+
+    public StaffUserResponseDto createSuperAdmin(StaffUserRequestDto staffUserRequestDto) {
+        if (staffUserRepository.findByEmail(staffUserRequestDto.getEmail()).isPresent()) {
+            throw new RuntimeException("Email already exists.");
+        }
+
+        // Create StaffUser
+        StaffUser superAdmin = StaffUser.builder()
+                .fullName(staffUserRequestDto.getFullName())
+                .email(staffUserRequestDto.getEmail())
+                .role(Role.SUPERADMIN)
+                .department(staffUserRequestDto.getDepartment())
+                .designation(staffUserRequestDto.getDesignation())
+                .phoneNumber(staffUserRequestDto.getPhoneNumber())
+                .password(passwordEncoder.encode(staffUserRequestDto.getPassword()))
+                .createdAt(LocalDateTime.now())
+                .district(staffUserRequestDto.getDistrict())
+                .municipality(staffUserRequestDto.getMunicipality())
+                .status(Status.ACTIVE)
+                .addedBy(staffUserRequestDto.getAddedBy())
+                .build();
+
+        StaffUser savedStaff = staffUserRepository.save(superAdmin);
+
+        // Create corresponding User
+        String token = jwtUtil.generateToken(savedStaff.getEmail());
+
+        User user = User.builder()
+                .email(savedStaff.getEmail())
+                .password(savedStaff.getPassword())
+                .role(savedStaff.getRole())
+                .staffUser(savedStaff)
+                .createdAt(LocalDateTime.now())
+                .jwtToken(token)
+                .build();
+
+        userRepository.save(user);
+
+        return StaffUserDtoMapper.staffUserResponseDto(savedStaff);
+    }
 }
