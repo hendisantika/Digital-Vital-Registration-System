b4e6454737b209b669e6cb35942ec91048ffe9fe
hendisantika <hendisantika@yahoo.co.id>
2025-08-26 06:06:33 +0700

    feat: Add UserResponseDto🫸🌀✏️📗🐧🐳

--- src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/user/dto/UserResponseDto.java
+++ src/main/java/id/my/hendisantika/digitalvitalregistrationsystem/user/dto/UserResponseDto.java
@@ -1,12 +1,14 @@
 package id.my.hendisantika.digitalvitalregistrationsystem.user.dto;
 
 import id.my.hendisantika.digitalvitalregistrationsystem.staff.enums.Role;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,16 +18,20 @@ import java.time.LocalDate;
  * Email: hendisantika@yahoo.co.id
  * Telegram : @hendisantika34
  * Date: 26/08/25
- * Time: 06.05
+ * Time: 06.06
  * To change this template use File | Settings | File Templates.
  */
 @Data
-@Builder
-@NoArgsConstructor
 @AllArgsConstructor
-public class UserRequestDto {
+@NoArgsConstructor
+@Builder
+public class UserResponseDto {
+    private Long id;
     private String email;
-    private String password;
+    private String jwtToken;
+    @Enumerated(EnumType.STRING)
     private Role role;
-    private LocalDate createdAt;
+    private LocalDateTime createdAt;
+    private String department;
+    private String municipality;
 }
